9c28096f1ee5e6205167c479728274b08486f55d
vignesh <vignesh@kiebot.com>
2024-02-16 20:49:25 +0530

    test

---
+++ Test.java
@@ -0,0 +1,14 @@
+
+
+    
+
+
+class Test{
+    public static void testing(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args){
+        Test.testing();
+
+    }
+}
