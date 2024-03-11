f65737d03dbc3c922c9366819f4313f4db8bf583
vignesh <vignesh@kiebot.com>
2024-03-11 22:21:06 +0530

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import './App.css';
-import Test from './Test'
-import DynamicTable from './DynamicTable'
+import Test from './Test';
+import DynamicTable from './DynamicTable';
 import SingleCalc from './SingleCalc';
-import MyComponent from './MyComponent'
+import MyComponent from './MyComponent';
 import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Link to="/test" className="styled-link">test</Link>
-<Link to="/single" className="styled-link">single</Link>
-<Link to="/d" className="styled-link">DynamicTable</Link>
-<Link to="/mycomponent" className="styled-link">mycomponent</Link>
+      <Link to="/test" className="styled-link">
+        test
+      </Link>
+      <Link to="/single" className="styled-link">
+        single
+      </Link>
+      <Link to="/d" className="styled-link">
+        DynamicTable
+      </Link>
+      <Link to="/mycomponent" className="styled-link">
+        mycomponent
+      </Link>
 
-    <div className="App">
-      <header className="App-header">
-
-    <Routes>
-      <Route path='/test' element={<Test/>}/>
-      <Route path='/single' element={<SingleCalc/>}/>
-      <Route path='/d' element={<DynamicTable/>}/>
-      <Route path='/mycomponent' element={<MyComponent/>}/>
-    </Routes>
-      </header>
-    </div>
-
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/test" element={<Test />} />
+            <Route path="/single" element={<SingleCalc />} />
+            <Route path="/d" element={<DynamicTable />} />
+            <Route path="/mycomponent" element={<MyComponent />} />
+          </Routes>
+        </header>
+      </div>
     </>
-
   );
 }
 
